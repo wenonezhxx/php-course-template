06be158b00b8de34340621a8e576b27d71c64229
wenonezhxx <vadim.p.11111@gmail.com>
2025-10-11 09:52:03 +0000

    lab5 недодел

--- sites/skuit/www/index.php
+++ sites/skuit/www/index.php
@@ -1,5 +1,5 @@
 <?
-include_once './lab4/header.php';
-include_once './lab4/hobits.php';
-include_once './lab4/footer.php';
+include_once './lab5/header.php';
+include_once './lab5/main.php';
+include_once './lab5/footer.php';
 ?>
