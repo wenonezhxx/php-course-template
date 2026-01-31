842d4663659b44d8a9fd9e7a2cb9cf22a95f6f08
wenonezhxx <vadim.p.11111@gmail.com>
2026-01-31 08:46:21 +0000

    lab9

--- sites/skuit/www/index.php
+++ sites/skuit/www/index.php
@@ -1,3 +1,3 @@
 <?
-include_once 'sites/skuit/www/lab9/main.php'
+include_once '/workspaces/php-course-template/sites/skuit/www/lab9/main.php'
 ?>
--- sites/skuit/www/lab9/main.php
+++ sites/skuit/www/lab9/main.php
@@ -1,33 +1,68 @@
 <?php
-class Book {
+class Book 
+{
+    public static $count;
     public string $title;
     public string $author;
     public int $year;
 
-    public function getInfo(): string {
-        return "Название: {$this->title}, Автор: {$this->author}, Год: {$this->year}";
-    }
-}
-
-$book1 = new Book();
-$book1->title = "Преступление и наказание";
-$book1->author = "Фёдор Достоевский";
-$book1->year = 1866;
-
-$book2 = new Book();
-$book2->title = "Мастер и Маргарита";
-$book2->author = "Михаил Булгаков";
-$book2->year = 1967;
-
-echo $book1->getInfo() . "\n";
-echo $book2->getInfo() . "\n";
-
-class BookWithConstructor extends Book {
-    public function __construct(string $title, string $author, int $year) {
+    public function __construct(string $title, string $author, int $year) 
+    {
         $this->title = $title;
         $this->author = $author;
         $this->year = $year;
     }
+
+    public function getInfo(): string 
+    {
+        return "Название: {$this->title}, Автор: {$this->author}, Год: {$this->year}";
+    }
+    public function isOld(): bool 
+    {
+        $currentYear = date('Y');
+        return ($currentYear - $this->year) > 50;
+    }
 }
-$book3 = new BookWithConstructor("1984", "Джордж Оруэлл", 1949);
-echo $book3->getInfo() . "\n";
+$books = [
+    new Book("Преступление и наказание", "Фёдор Достоевский", 1866),
+    new Book("Мастер и Маргарита", "Михаил Булгаков", 1967),
+    new Book("1984", "Джордж Оруэлл", 1949)
+];
+
+foreach ($books as $book) {
+    echo $book->getInfo() . "\n <br>";
+    
+    if ($book->isOld()) {
+        echo "Книга '{$book->title}' старая\n <br>";
+    } else {
+        echo "Книга '{$book->title}' не старая\n <br>";
+    }
+    echo "---\n <br>";
+}
+class Car 
+{
+    public string $title;
+    public string $color;
+    private int $year;
+
+    public function __construct(string $title, string $color, int $year) {
+        $this->title = $title;
+        $this->color = $color;  
+        $this->year = $year;
+    }
+
+    public function getInfo(): string 
+    {
+        return "Автомобиль: {$this->title} <br>, Цвет: {$this->color} <br>, Год выпуска: {$this->year} <br>";
+    }
+}
+
+$cars = [
+    new Car("Toyota Camry", "черный", 2020),
+    new Car("BMW X5", "белый", 2015),
+    new Car("Lada Vesta", "красный", 2022)
+];
+
+foreach ($cars as $car) {
+    echo $car->getInfo() . "\n <br>";
+}
